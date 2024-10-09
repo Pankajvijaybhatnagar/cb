41170a73baf9444bed935acab0b186e2c103538a
ankitjnvynr <ankitbkana@outlook.com>
2024-10-09 00:12:37 -0700

    update header

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,116 +1,229 @@
-import React from 'react'
-import Link from 'next/link'
-import MobileMenu from '../MobileMenu/MobileMenu'
-import Image from 'next/image'
+import React from "react";
+import Link from "next/link";
+import MobileMenu from "../MobileMenu/MobileMenu";
+import Image from "next/image";
 
 const Header = (props) => {
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
-
-    return (
-        <header id="header" >
-            <div className={" " + props.hclass}>
-                <nav className="navigation navbar navbar-expand-lg navbar-light">
-                    <div className="container-fluid">
-                        <div className="row align-items-center">
-                            <div className="col-lg-3 col-md-3 col-3 d-lg-none dl-block">
-                                <MobileMenu />
-                            </div>
-                            <div className="col-lg-2 col-md-6 col-6">
-                                <div className="navbar-header">
-                                    <Link className="navbar-brand" href="/home">
-                                        <Image src={props.Logo} alt="logo" />
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-lg-7 col-md-1 col-1">
-                                <div id="navbar" className="collapse navbar-collapse navigation-holder">
-                                    <ul className="nav navbar-nav mb-2 mb-lg-0">
-                                        <li className="menu-item-has-children">
-                                            <Link onClick={ClickHandler}  href="/">Home</Link>
-                                            {/* <ul className="sub-menu">
-                                                <li><Link onClick={ClickHandler}  href="/">Home style 1</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/home-2">Home style 2</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/home-3">Home style 3</Link></li>
-                                            </ul> */}
-                                        </li>
-                                        <li><Link onClick={ClickHandler} href="/">About</Link></li>
-                                        <li className="menu-item-has-children">
-                                            <Link onClick={ClickHandler} href="#">Services</Link>
-                                            <ul className="sub-menu">
-                                                <li><Link onClick={ClickHandler} href="/">Services</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/">Services Single</Link></li>
-                                            </ul>
-                                        </li>
-                                        <li className="menu-item-has-children">
-                                            <Link onClick={ClickHandler} href="#">Admission</Link>
-                                            <ul className="sub-menu">
-                                                <li><Link onClick={ClickHandler} href="/">Eligibility</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/">Admission Procedure</Link></li>
-                                            </ul>
-                                        </li>
-                                        <li className="menu-item-has-children">
-                                            <Link onClick={ClickHandler} href="#">Facilities</Link>
-                                            {/* <ul className="sub-menu">
-                                                <li><Link onClick={ClickHandler} href="/blog">Blog right sidebar</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/blog-left-sidebar">Blog left sidebar</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/blog-fullwidth">Blog fullwidth</Link></li>
-                                                <li className="menu-item-has-children">
-                                                    <Link onClick={ClickHandler} href="#">Blog details</Link>
-                                                    <ul className="sub-menu">
-                                                        <li><Link onClick={ClickHandler} href="/blog-single/15-Secrets-Of-Digital-Transformation">Blog details right sidebar</Link>
-                                                        </li>
-                                                        <li><Link onClick={ClickHandler} href="/blog-single-left-sidebar/15-Secrets-Of-Digital-Transformation">Blog details left
-                                                            sidebar</Link></li>
-                                                        <li><Link onClick={ClickHandler} href="/blog-single-fullwidth/15-Secrets-Of-Digital-Transformation">Blog details
-                                                            fullwidth</Link></li>
-                                                    </ul>
-                                                </li>
-                                            </ul> */}
-                                        </li>
-                                        <li className="menu-item-has-children">
-                                            <Link onClick={ClickHandler} href="#">Career</Link>
-                                            {/* <ul className="sub-menu">
-                                                <li><Link onClick={ClickHandler} href="/testimonials">testimonials</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/login">Login</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/register">Register</Link></li>
-                                                <li><Link onClick={ClickHandler} href="/404">404 Error</Link></li>
-                                            </ul> */}
-                                        </li>
-                                        <li><Link onClick={ClickHandler} href="/contact">Contact-us</Link></li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-md-2 col-2">
-                                <div className="header-right">
-                                    <a href="tel:8708042248" className="call-number">
-                                        <div className="icon">
-                                            <Image src={props.telephone} alt="" />
-                                        </div>
-                                        <div className="text">
-                                            <span className="title">Available 24/7</span>
-                                            <span className="number">+(91)8708042248</span>
-                                        </div>
-                                    </a>
-                                    <div className="login">
-                                        <Link href="/login">Login</Link>
-                                    </div>
-                                    <div className="close-form">
-                                        <Link className="theme-btn" href="/contact">Start Free Trial</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <header id="header">
+      <div className={" " + props.hclass}>
+        <nav className="navigation navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <div className="row items-center">
+              <div className="col-lg-3 col-md-3 col-3 d-lg-none block">
+                <MobileMenu />
+              </div>
+              <div className="col-lg-2 col-md-6 col-6">
+                <div className="navbar-header">
+                  <Link className="navbar-brand  " href="/home">
+                    <Image src={props.Logo} alt="logo" />
+                  </Link>
+                </div>
+              </div>
+              <div className="col-lg-7 col-md-1 col-1">
+                <div
+                  id="navbar"
+                  className="collapse navbar-collapse navigation-holder"
+                >
+                  <ul className="nav navbar-nav mb-2 mb-lg-0 text-sm">
+                    <li>
+                      <Link onClick={ClickHandler} href="/" className="text-xs">
+                        Home
+                      </Link>
+                    </li>
+                    <li className="menu-item-has-children">
+                      <Link onClick={ClickHandler} href="/" className="text-xs">
+                        About
+                      </Link>
+                      <ul className="sub-menu text-xs">
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Services
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Services Single
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Services Single
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Services Single
+                          </Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-has-children">
+                      <Link onClick={ClickHandler} href="#" className="text-xs">
+                        Alumni
+                      </Link>
+                      <ul className="sub-menu text-xs">
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Registration form
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Alumni Testimonials
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Alumni videos
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Gallery
+                          </Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-has-children">
+                      <Link onClick={ClickHandler} href="#" className="text-xs">
+                        Admission
+                      </Link>
+                      <ul className="sub-menu text-xs">
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Eligibility
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/">
+                            Admission Procedure
+                          </Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-has-children">
+                      <Link onClick={ClickHandler} href="#" className="text-xs">
+                        Facilities
+                      </Link>
+                      <ul className="sub-menu text-xs">
+                        <li>
+                          <Link onClick={ClickHandler} href="/blog">
+                            Blog right sidebar
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            onClick={ClickHandler}
+                            href="/blog-left-sidebar"
+                          >
+                            Blog left sidebar
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/blog-fullwidth">
+                            Blog fullwidth
+                          </Link>
+                        </li>
+                        <li className="menu-item-has-children">
+                          <Link onClick={ClickHandler} href="#" className="text-xs">
+                            Blog details
+                          </Link>
+                          <ul className="sub-menu text-xs">
+                            <li>
+                              <Link
+                                onClick={ClickHandler}
+                                href="/blog-single/15-Secrets-Of-Digital-Transformation"
+                              >
+                                Blog details right sidebar
+                              </Link>
+                            </li>
+                            <li>
+                              <Link
+                                onClick={ClickHandler}
+                                href="/blog-single-left-sidebar/15-Secrets-Of-Digital-Transformation"
+                              >
+                                Blog details left sidebar
+                              </Link>
+                            </li>
+                            <li>
+                              <Link
+                                onClick={ClickHandler}
+                                href="/blog-single-fullwidth/15-Secrets-Of-Digital-Transformation"
+                              >
+                                Blog details fullwidth
+                              </Link>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="menu-item-has-children">
+                      <Link onClick={ClickHandler} href="#" className="text-xs">
+                        Career
+                      </Link>
+                      <ul className="sub-menu text-xs">
+                        <li>
+                          <Link onClick={ClickHandler} href="/testimonials">
+                            Testimonials
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/login">
+                            Login
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/register">
+                            Register
+                          </Link>
+                        </li>
+                        <li>
+                          <Link onClick={ClickHandler} href="/404">
+                            404 Error
+                          </Link>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <Link onClick={ClickHandler} href="/contact" className="text-xs">
+                        Contact Us
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-2 col-2">
+                <div className="header-right">
+                  <a href="tel:8708042248" className="call-number text-xs">
+                    <div className="icon">
+                      <Image src={props.telephone} alt="" />
                     </div>
-                </nav>
+                    <div className="text">
+                      <span className="title text-xs">Available 24/7</span>
+                      <span className="number text-xs">+(91)8708042248</span>
+                    </div>
+                  </a>
+                  <div className="login">
+                    <Link href="/login" className="text-xs">Login</Link>
+                  </div>
+                  <div className="close-form">
+                    <Link className="theme-btn text-xs" href="/contact">
+                      Start Free Trial
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
-        </header>
-    )
-}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
-
-
